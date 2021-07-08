0899ae3d2a48da018a7abcc5c129a6761349cdae
Leszek <leszek_grechowicz@o2.pl>
2021-07-08 21:04:41 +0100

    06_wc add flag option

--- 06_wc/test.py
+++ 06_wc/test.py
@@ -92,8 +92,8 @@ def test_more():
 
     rv, out = getstatusoutput(f'{prg} {fox} {sonnet}')
     expected = ('       1       9      45 ../inputs/fox.txt\n'
-                '      17     118     661 ../inputs/sonnet-29.txt\n'
-                '      18     127     706 total')
+                '      17     118     668 ../inputs/sonnet-29.txt\n'
+                '      18     127     713 total')
     assert rv == 0
     assert out.rstrip() == expected
 
@@ -105,3 +105,73 @@ def test_stdin():
     rv, out = getstatusoutput(f'{prg} < {fox}')
     assert rv == 0
     assert out.rstrip() == '       1       9      45 <stdin>'
+
+
+# --------------------------------------------------
+def test_fox_char():
+    """Test on fox with flag -c"""
+
+    for flag in ['-c', '--char']:
+        rv, out = getstatusoutput(f'{prg} {fox} {flag}')
+        assert rv == 0
+        assert out.rstrip() == '      45 ../inputs/fox.txt'
+
+
+# --------------------------------------------------
+def test_fox_word():
+    """Test on fox with flag -w"""
+
+    for flag in ['-w', '--word']:
+        rv, out = getstatusoutput(f'{prg} {fox} {flag}')
+        assert rv == 0
+        assert out.rstrip() == '       9 ../inputs/fox.txt'
+
+
+# --------------------------------------------------
+def test_fox_lines():
+    """Test on fox with flag -l"""
+
+    for flag in ['-l', '--lines']:
+        rv, out = getstatusoutput(f'{prg} {fox} {flag}')
+        assert rv == 0
+        assert out.rstrip() == '       1 ../inputs/fox.txt'
+
+
+# --------------------------------------------------
+def test_fox_mix_flag_lc():
+    """Test on fox with mix flag -lc"""
+
+    for flag in ['-cl', '-lc']:
+        rv, out = getstatusoutput(f'{prg} {fox} {flag}')
+        assert rv == 0
+        assert out.rstrip() == '       1      45 ../inputs/fox.txt'
+
+
+# --------------------------------------------------
+def test_fox_mix_flag_cw():
+    """Test on fox with mix flag -cw"""
+
+    for flag in ['-cw', '-wc']:
+        rv, out = getstatusoutput(f'{prg} {fox} {flag}')
+        assert rv == 0
+        assert out.rstrip() == '       9      45 ../inputs/fox.txt'
+
+
+# --------------------------------------------------
+def test_fox_mix_flag_lw():
+    """Test on fox with mix flag -lw"""
+
+    for flag in ['-wl', '-lw']:
+        rv, out = getstatusoutput(f'{prg} {fox} {flag}')
+        assert rv == 0
+        assert out.rstrip() == '       1       9 ../inputs/fox.txt'
+
+
+# --------------------------------------------------
+def test_fox_all_flag():
+    """Test on fox with flag -clw"""
+
+    for flag in ['-cwl', '-wcl', '-wlc', '-lwc', '-lcw', '-clw']:
+        rv, out = getstatusoutput(f'{prg} {fox} {flag}')
+        assert rv == 0
+        assert out.rstrip() == '       1       9      45 ../inputs/fox.txt'
--- 06_wc/wc.py
+++ 06_wc/wc.py
@@ -24,12 +24,28 @@ def get_args():
                         type=argparse.FileType('rt'),
                         default=[sys.stdin])
 
+    parser.add_argument('-c',
+                        '--char',
+                        help='number of characters',
+                        action='store_true')
+
+    parser.add_argument('-l',
+                        '--lines',
+                        help='number of lines',
+                        action='store_true')
+
+    parser.add_argument('-w',
+                        '--words',
+                        help='number of words',
+                        action='store_true')
+
     return parser.parse_args()
 
 
 # --------------------------------------------------
+
 def main():
-    """Make a jazz noise here"""
+    """Count lines, words and bytes of the text from the file"""
 
     args = get_args()
     sum_bytes, sum_lines, sum_words = 0, 0, 0
@@ -42,14 +58,29 @@ def main():
             text_bytes += len(line)
             text_words += len(line.split())
 
-        print(f'{text_lines:>{8}}{text_words:>{8}}{text_bytes:>{8}} {file.name}')
+        if any([args.lines, args.words, args.char]):
+            report = ''
+
+            if args.lines:
+                report += f'{text_lines:>{8}}'
+
+            if args.words:
+                report += f'{text_words:>{8}}'
+
+            if args.char:
+                report += f'{text_bytes:>{8}}'
+
+            print(report + f' {file.name}')
+
+        else:
+            print(f'{text_lines:>{8}}{text_words:>{8}}{text_bytes:>{8}} {file.name}')
 
         sum_bytes += text_bytes
         sum_lines += text_lines
         sum_words += text_words
 
     if len(args.file) > 1:
-        print(f'{sum_lines:>{8}}{sum_words:>{8}}{sum_bytes:>{7}} total')
+        print(f'{sum_lines:>{8}}{sum_words:>{8}}{sum_bytes:>{8}} total')
 
 
 # --------------------------------------------------
